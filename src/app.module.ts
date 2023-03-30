8764b72471490eb549264b5ebcaf1db3c7994436
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-03-30 18:29:27 -0500

    [add] agregando autenticacion de usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './modules/auth.module'; // Asegúrate de que la ruta de importación sea correcta
+import { User } from './entities/user.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'myusername',
+      password: 'mypassword',
+      database: 'digital-seller',
+      entities: [User],
+      synchronize: true,
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
---
+++ src/services/user.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../entities/user.entity';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { LoginUserDto } from '../dto/login-user.dto';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async register(createUserDto: CreateUserDto) {
+    const user = new User();
+    user.username = createUserDto.username;
+    const salt = await bcrypt.genSalt(); // generate a salt
+    user.password = await bcrypt.hash(createUserDto.password, salt); // hash the password with the salt
+    await this.userRepository.save(user);
+    return user;
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const user = await this.userRepository.findOne({
+      where: {
+        username: loginUserDto.username,
+      },
+    });
+
+    if (!user) {
+      throw new Error('Invalid credentials');
+    }
+
+    const passwordMatch = await bcrypt.compare(
+      loginUserDto.password,
+      user.password,
+    ); // compare the provided password with the stored hash
+
+    if (!passwordMatch) {
+      throw new Error('Invalid credentials');
+    }
+
+    const payload = { username: user.username, sub: user.id };
+    const token = this.jwtService.sign(payload);
+    return { token };
+  }
+}
