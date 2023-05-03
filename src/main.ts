fb7e3699bf302d242c09e83df0bc265314222339
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-05-02 22:48:25 -0500

    [debug] corrigiendo errores con CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,22 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigins = ['http://localhost:5173'];
+
+  // Habilitar CORS
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
+  });
+
   await app.listen(4004);
 }
 
